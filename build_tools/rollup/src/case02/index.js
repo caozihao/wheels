00d4d95680495abc8e4d985ec1fe1bd495a91378
caozihao <caozihao@rcrai.com>
2021-07-10 21:00:38 +0800

    添加rollup case

---
+++ build_tools/rollup/src/case02/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: caozihao
+ * @Date: 2021-07-10 19:46:32
+ * @LastEditors: caozihao
+ * @LastEditTime: 2021-07-10 20:17:31
+ */
+
+import { add, xx } from './add';
+import { minus } from './minus';
+
+function hello() {
+	return 'hello';
+}
+const result = minus(4, 2);
+
+alert(result);
+console.log(add(2, 4));
